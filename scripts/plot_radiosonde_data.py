16846db4c326fff3e56ba91e2908d9c666a0fd58
laura.koehler <laura.koehler@mpimet.mpg.de>
2023-10-09 11:31:49 +0200

    Unit-aware functions for plotting script
    The script now uses unit-aware function for all plots to avoid that variables have to be given in specific units in the input data.

--- scripts/plot_radiosonde_data.py
+++ scripts/plot_radiosonde_data.py
@@ -19,15 +19,16 @@ This script produces 3 plots:
 
 import argparse
 import os.path
+from decimal import ROUND_HALF_UP, Decimal
 
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import metpy.calc as mpcalc
 import numpy as np
+import pint_xarray  # noqa: F401
 import xarray as xr
 from metpy.plots import Hodograph, SkewT
 from metpy.units import units
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 
 def get_args():
@@ -64,6 +65,47 @@ def get_args():
     return parsed_args
 
 
+def data4skewt(direction, data):
+    if direction == "ascent":
+        p = data["p"].isel({"sounding": 0}).pint.to(units.Pa).values * units.Pa
+        T = data["ta"].isel({"sounding": 0}).pint.to(units.K).values * units.K
+        Td = data["dp"].isel({"sounding": 0}).pint.to(units.K).values * units.K
+        wind_speed = (
+            data["wspd"]
+            .isel({"sounding": 0})
+            .pint.to(units.meter / units.second)
+            .values
+            * units.meter
+            / units.second
+        )
+        wind_dir = (
+            data["wdir"].isel({"sounding": 0}).pint.to(units.degrees).values
+            * units.degrees
+        )
+        u, v = mpcalc.wind_components(wind_speed, wind_dir)
+
+    elif direction == "descent":
+        p = np.flip(data["p"].isel({"sounding": 0}).pint.to(units.Pa).values) * units.Pa
+        T = np.flip(data["ta"].isel({"sounding": 0}).pint.to(units.K).values) * units.K
+        Td = np.flip(data["dp"].isel({"sounding": 0}).pint.to(units.K).values) * units.K
+        wind_speed = (
+            np.flip(
+                data["wspd"]
+                .isel({"sounding": 0})
+                .pint.to(units.meter / units.second)
+                .values
+            )
+            * units.meter
+            / units.second
+        )
+        wind_dir = (
+            np.flip(data["wdir"].isel({"sounding": 0}).pint.to(units.degrees).values)
+            * units.degrees
+        )
+        u, v = mpcalc.wind_components(wind_speed, wind_dir)
+    return p, T, Td, wind_speed, wind_dir, u, v
+
+
 def main():
     args = get_args()
 
@@ -71,6 +113,9 @@ def main():
     filename = args["inputfile"]
 
     data = xr.open_dataset(filename)
+    data = data.metpy.quantify()
+    for coord_name in data.coords:
+        data.coords[coord_name] = data.coords[coord_name].metpy.quantify()
     if "ascent" in filename:
         direction = "ascent"
     elif "descent" in filename:
@@ -96,6 +141,12 @@ def main():
     launch_time = np.datetime64(data.launch_time.values[0], "m")
     timestamp = str(np.datetime64(launch_time, "m")).replace(":", "")
 
+    lats = data.isel(sounding=snd).lat.pint.to("degree_north")
+    lons = data.isel(sounding=snd).lon.pint.to("degree_east")
+    alts = (
+        (data.isel(sounding=snd).alt_WGS84).pint.to(units.km).pint.dequantify().values
+    )
+
     props = dict(boxstyle="round", facecolor="white", alpha=0.8)
 
     # Trajectory of the radiosonde
@@ -112,24 +163,25 @@ def main():
         linestyle="--",
     )
     traj = plt.scatter(
-        data.isel(sounding=snd).lon,
-        data.isel(sounding=snd).lat,
-        c=(data.isel(sounding=snd).alt_WGS84.values) / 1000,
+        lons,
+        lats,
+        c=alts,
         s=1,
         cmap="hot",
     )
     plt.colorbar(traj, label="altitude (km)", pad=0.06)
-    lon_min = data.lon.min()
-    lon_max = data.lon.max()
+    lon_min = lons.min().values
+    lon_max = lons.max().values
     ax.set_xlim([lon_min - 0.3, lon_max + 0.3])
-    lat_min = data.lat.min()
-    lat_max = data.lat.max()
+    lat_min = lats.min().values
+    lat_max = lats.max().values
     ax.set_ylim([lat_min - 0.3, lat_max + 0.3])
     ax.set_title(
         f"Radiosonde trajectory: launch time: {launch_time}, direction: {direction}",
         fontsize=13,
     )
-    alt_max = round(data.isel(sounding=snd).alt_WGS84.values.max() / 1000, 1)
+    alt_max = alts.max().round(1)
+    alt_max = Decimal(str(alt_max)).quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
     ax.text(
         0.02,
         0.07,
@@ -149,35 +201,33 @@ def main():
     )
 
     ax[0].plot(
-        data.isel(sounding=snd).ta,
-        data.isel(sounding=snd).alt_WGS84 / 1000,
+        data.isel(sounding=snd).ta.pint.to(units.K),
+        alts,
         color=quantities_color,
     )
     ax[1].plot(
-        data.isel(sounding=snd).dp,
-        data.isel(sounding=snd).alt_WGS84 / 1000,
+        data.isel(sounding=snd).dp.pint.to(units.K),
+        alts,
         color=quantities_color,
     )
     ax[2].plot(
-        data.isel(sounding=snd).rh * 100,
-        data.isel(sounding=snd).alt_WGS84 / 1000,
+        data.isel(sounding=snd).rh.pint.to(units.percent),
+        alts,
         color=quantities_color,
     )
     ax[3].plot(
-        data.isel(sounding=snd).mr * 100,
-        data.isel(sounding=snd).alt_WGS84 / 1000,
+        data.isel(sounding=snd).mr.pint.to(units.percent),
+        alts,
         color=quantities_color,
     )
     ax[4].plot(
-        (data.isel(sounding=snd).wspd.values * units.knots).to(
-            units.meter / units.second
-        ),
-        data.isel(sounding=snd).alt_WGS84 / 1000,
+        data.isel(sounding=snd).wspd.pint.to(units.meter / units.second),
+        alts,
         color=quantities_color,
     )
     ax[5].plot(
-        data.isel(sounding=snd).wdir,
-        data.isel(sounding=snd).alt_WGS84 / 1000,
+        data.isel(sounding=snd).wdir.pint.to(units.degrees),
+        alts,
         color=quantities_color,
     )
 
@@ -193,7 +243,7 @@ def main():
     ax[1].set_xlabel(r"$\tau$ (K)", fontsize=13)
     ax[3].set_xlabel(r"mixing ratio (%)", fontsize=13)
     ax[4].set_xlabel("wind speed (m/s)", fontsize=13)
-    ax[5].set_xlabel("wind direction (degrees)", fontsize=13)
+    ax[5].set_xlabel("wind direction (degree)", fontsize=13)
     fig_quantities.suptitle(
         f"Radiosonde observations: launch time: {launch_time}, direction: {direction}",
         fontsize=14,
@@ -207,27 +257,7 @@ def main():
     if direction == "unknown":
         print("No SkewT plot created since direction unknown.")
     else:
-        if direction == "ascent":
-            p = data["p"].isel({"sounding": 0}).values * units.Pa
-            T = data["ta"].isel({"sounding": 0}).values * units.K
-            Td = data["dp"].isel({"sounding": 0}).values * units.K
-            wind_speed = data["wspd"].isel({"sounding": 0}).values * units.knots
-            wind_speed = wind_speed.to(units.meter / units.second)
-            wind_dir = data["wdir"].isel({"sounding": 0}).values * units.degrees
-            u, v = mpcalc.wind_components(wind_speed, wind_dir)
-
-        elif direction == "descent":
-            p = np.flip(data["p"].isel({"sounding": 0}).values) * units.Pa
-            T = np.flip(data["ta"].isel({"sounding": 0}).values) * units.K
-            Td = np.flip(data["dp"].isel({"sounding": 0}).values) * units.K
-            wind_speed = (
-                np.flip(data["wspd"].isel({"sounding": 0}).values) * units.knots
-            )
-            wind_speed = wind_speed.to(units.meter / units.second)
-            wind_dir = (
-                np.flip(data["wdir"].isel({"sounding": 0}).values) * units.degrees
-            )
-            u, v = mpcalc.wind_components(wind_speed, wind_dir)
+        p, T, Td, wind_speed, wind_dir, u, v = data4skewt(direction, data)
 
         lcl_pressure, lcl_temperature = mpcalc.lcl(p[0], T[0], Td[0])
 
@@ -266,8 +296,9 @@ def main():
         # Search for levels by providing pressures
         # (levels is the coordinate not pressure)
         pres_vals = (
-            data["p"].isel({"sounding": 0}).values / 100
-        )  # Convertion from Pa to hPa
+            data["p"].isel({"sounding": 0}).pint.to(units.hPa)
+        ).values  # Convertion from Pa to hPa
+
         closest_pressure_levels = np.unique(
             [
                 find_nearest(pres_vals[~np.isnan(pres_vals)], p_)
@@ -372,9 +403,7 @@ def main():
         skew.plot_mixing_lines()
 
         # Create a hodograph
-        # Create an inset axes object that is 40% width and height of the
-        # figure and put it in the upper right hand corner.
-        ax_hod = inset_axes(skew.ax, "47%", "40%", loc=1)
+        ax_hod = fig.add_axes([0.55, 0.54, 0.29, 0.29])
         h = Hodograph(ax_hod, component_range=80.0)
         h.add_grid(increment=20)
         h.plot_colormapped(u, v, wind_speed)  # Plot a line colored by wind speed
@@ -410,7 +439,9 @@ def main():
         )
         fig_skewt = fig
         fig_skewt.savefig(
-            f"{outdir}/SkewT/skewT_{timestamp}_{direction}.png", bbox_inches="tight"
+            f"{outdir}/SkewT/skewT_{timestamp}_{direction}.png",
+            bbox_inches="tight",
+            pad_inches=0.1,
         )
 
 
